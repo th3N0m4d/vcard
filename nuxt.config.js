cc289b322ae903f8c46ec31dfd80f0547db13388
th3N0m4d <edielton.dantas@hotmail.com>
2021-06-04 09:16:24 +0200

    feat(assets): add stlyes, images and scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,42 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Pacifico',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Quicksand:400,700',
+      },
+    ],
+    script: [
+      {
+        src: 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/fonts/icomoon/style.css',
+    '@/assets/css/plugins.min.css',
+    '@/assets/css/style.css',
+  ],
+
+  script: [
+    {
+      src: '@/assets/js/plugins.min.js',
+    },
+    {
+      src: '@/assets/js/theme.min.js',
+    },
+    {
+      src: '@/assets/js/vendor/modernizr-3.3.1.min.js',
+    },
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
