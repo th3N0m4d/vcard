da3cf497e2afed419214a05805f121ad9f5f5482
th3N0m4d <edielton.dantas@hotmail.com>
2021-06-15 17:38:11 +0200

    feat(favicon): add favicon to website

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,27 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        type: 'apple-touch-icon',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'icon',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'icon',
+        href: '/favicon/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/favicon/site.webmanifest',
+      },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Pacifico',
